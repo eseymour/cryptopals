002dd3ffcf9bc730bc15f8aa62291b61735cd08e
Edward Seymour <dev@edwardseymour.me>
2018-01-19 21:32:00 -0700

    Add zero value test case to challenge 1

--- set1/challenge01/hexToBase64/hexToBase64_test.go
+++ set1/challenge01/hexToBase64/hexToBase64_test.go
@@ -8,6 +8,11 @@ func TestHexToBase64(t *testing.T) {
 		fails    bool
 	}{
 		{
+			"",
+			"",
+			false,
+		},
+		{
 			"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
 			"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
 			false,
@@ -21,9 +26,10 @@ func TestHexToBase64(t *testing.T) {
 
 	for _, c := range cases {
 		got, err := HexToBase64(c.in)
+		failed := err != nil
 		switch {
-		case (err != nil) != c.fails:
-			t.Errorf("HexToBase64(%#v) errors: %#v, want %#v", c.in, err != nil, c.fails)
+		case failed != c.fails:
+			t.Errorf("HexToBase64(%#v) failure: %#v, want %#v", c.in, failed, c.fails)
 		case !c.fails && got != c.want:
 			t.Errorf("HexToBase64(%#v) == %#v,  want %#v", c.in, got, c.want)
 		}
