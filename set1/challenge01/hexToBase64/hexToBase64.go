eea9367a5d9c02be715e301b41aa71071d4f242e
Edward Seymour <dev@edwardseymour.me>
2018-01-19 22:21:46 -0700

    Add documentation comments to exported functions

--- set1/challenge01/hexToBase64/hexToBase64.go
+++ set1/challenge01/hexToBase64/hexToBase64.go
@@ -5,6 +5,9 @@ import (
 	"encoding/hex"
 )
 
+// HexToBase64 decodes the hex encoded string hexStr returns it as a base 64
+// encoded string. HexToBase64 expects hexStr to contain only an even amount
+// of hexadecimal characters, failing otherwise.
 func HexToBase64(hexStr string) (base64Str string, err error) {
 	data, err := hex.DecodeString(hexStr)
 	if err != nil {
