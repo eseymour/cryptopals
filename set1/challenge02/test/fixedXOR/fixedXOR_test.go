6f5a5e38dee29b08d061dfe2b1b44fd38c8efa91
Edward Seymour <dev@edwardseymour.me>
2018-01-20 18:27:29 -0700

    Simplify challenge 2 tests

--- set1/challenge02/test/fixedXOR/fixedXOR_test.go
+++ set1/challenge02/test/fixedXOR/fixedXOR_test.go
@@ -6,41 +6,25 @@ import (
 	"testing"
 
 	"github.com/eseymour/cryptopals/pkg/crypto/xor"
+	b "github.com/eseymour/cryptopals/pkg/util/bytes"
 )
 
-// must is a helper that wraps calls returning ([]byte, error), panicking if the
-// error is non-nil.
-func must(data []byte, err error) []byte {
-	if err != nil {
-		panic(err)
-	}
-	return data
-}
-
-// TestFixedXOR only tests the case from the challenge. The
-// package github.com/eseymour/cryptopals/pkg/crypto/xor has more extensive
-// tests.
+// TestFixedXOR only tests the case from the challenge. The package
+// github.com/eseymour/cryptopals/pkg/crypto/xor has more extensive tests.
 func TestFixedXOR(t *testing.T) {
-	cases := []struct {
-		plaintext, key, want []byte
-		fails                bool
-	}{
-		{ // Challenge case
-			must(hex.DecodeString("1c0111001f010100061a024b53535009181c")),
-			must(hex.DecodeString("686974207468652062756c6c277320657965")),
-			must(hex.DecodeString("746865206b696420646f6e277420706c6179")),
-			false,
-		},
-	}
 
-	for _, c := range cases {
-		got, err := xor.EncryptFixedKey(c.plaintext, c.key)
-		failed := err != nil
-		switch {
-		case failed != c.fails:
-			t.Errorf("xor.EncryptFixedKey(%#v, %#v) failure: %#v, want %#v", c.plaintext, c.key, failed, c.fails)
-		case !c.fails && !bytes.Equal(got, c.want):
-			t.Errorf("xor.EncryptFixedKey(%#v, %#v) == %#v, want %#v", c.plaintext, c.key, got, c.want)
-		}
+	plaintext := b.Must(hex.DecodeString("1c0111001f010100061a024b53535009181c"))
+	key := b.Must(hex.DecodeString("686974207468652062756c6c277320657965"))
+	want := b.Must(hex.DecodeString("746865206b696420646f6e277420706c6179"))
+	fails := false
+
+	got, err := xor.EncryptFixedKey(plaintext, key)
+	failed := err != nil
+
+	switch {
+	case failed != fails:
+		t.Errorf("xor.EncryptFixedKey(%#v, %#v) failure: %#v, want %#v", plaintext, key, failed, fails)
+	case !failed && !bytes.Equal(got, want):
+		t.Errorf("xor.EncryptFixedKey(%#v, %#v) == %#v, want %#v", plaintext, key, got, want)
 	}
 }
