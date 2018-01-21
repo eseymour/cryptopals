2bb56645f3d20d3ef447c3871e8668fbd272f5ef
Edward Seymour <dev@edwardseymour.me>
2018-01-20 18:05:24 -0700

    Modify solution to challenge 2 to use shared package

--- set1/challenge02/test/fixedXOR/fixedXOR_test.go
+++ set1/challenge02/test/fixedXOR/fixedXOR_test.go
@@ -1,9 +1,11 @@
-package fixedXor
+package fixedXOR
 
 import (
 	"bytes"
 	"encoding/hex"
 	"testing"
+
+	"github.com/eseymour/cryptopals/pkg/crypto/xor"
 )
 
 // must is a helper that wraps calls returning ([]byte, error), panicking if the
@@ -15,39 +17,30 @@ func must(data []byte, err error) []byte {
 	return data
 }
 
-func TestFixedXor(t *testing.T) {
+// TestFixedXOR only tests the case from the challenge. The
+// package github.com/eseymour/cryptopals/pkg/crypto/xor has more extensive
+// tests.
+func TestFixedXOR(t *testing.T) {
 	cases := []struct {
 		plaintext, key, want []byte
 		fails                bool
 	}{
-		{ // Zero value case
-			[]byte{},
-			[]byte{},
-			[]byte{},
-			false,
-		},
 		{ // Challenge case
 			must(hex.DecodeString("1c0111001f010100061a024b53535009181c")),
 			must(hex.DecodeString("686974207468652062756c6c277320657965")),
 			must(hex.DecodeString("746865206b696420646f6e277420706c6179")),
 			false,
 		},
-		{ //
-			must(hex.DecodeString("deadbeef")),
-			must(hex.DecodeString("deface")),
-			must(hex.DecodeString("cafe")),
-			true,
-		},
 	}
 
 	for _, c := range cases {
-		got, err := FixedXor(c.plaintext, c.key)
+		got, err := xor.EncryptFixedKey(c.plaintext, c.key)
 		failed := err != nil
 		switch {
 		case failed != c.fails:
-			t.Errorf("FixedXor(%#v, %#v) failure: %#v, want %#v", c.plaintext, c.key, failed, c.fails)
+			t.Errorf("xor.EncryptFixedKey(%#v, %#v) failure: %#v, want %#v", c.plaintext, c.key, failed, c.fails)
 		case !c.fails && !bytes.Equal(got, c.want):
-			t.Errorf("FixedXor(%#v, %#v) == %#v, want %#v", c.plaintext, c.key, got, c.want)
+			t.Errorf("xor.EncryptFixedKey(%#v, %#v) == %#v, want %#v", c.plaintext, c.key, got, c.want)
 		}
 	}
 }
