dd055113ad40e8dfe6b44eae74b64b3b9dbbf928
Edward Seymour <dev@edwardseymour.me>
2018-02-04 16:49:19 -0700

    Update challenge 3 test

--- set1/challenge03/test/crackByteXOR/crackByteXOR_test.go
+++ set1/challenge03/test/crackByteXOR/crackByteXOR_test.go
@@ -4,22 +4,17 @@ import (
 	"encoding/hex"
 	"testing"
 
-	"gonum.org/v1/gonum/stat/distuv"
-
 	"github.com/eseymour/cryptopals/pkg/crypto/analysis"
 	"github.com/eseymour/cryptopals/pkg/util/bytes"
 )
 
 func TestCrackByteXOR(t *testing.T) {
 	ciphertext := bytes.Must(hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"))
-	confidence := 0.99
+	threshold := 0.10
 
-	_, chiSquare := analysis.BreakXOREncryptByteKey(ciphertext)
+	_, pValue := analysis.BreakXOREncryptByteKey(ciphertext)
 
-	chiSquaredDist := distuv.ChiSquared{25, nil}
-
-	p := chiSquaredDist.CDF(chiSquare)
-	if p > confidence {
-		t.Errorf("BreakXOREncryptByteKey(%x) key is likely not correct with p-value %f", ciphertext, p)
+	if pValue < threshold {
+		t.Errorf("BreakXOREncryptByteKey(%x) key is likely not correct with p-value %f", ciphertext, pValue)
 	}
 }
